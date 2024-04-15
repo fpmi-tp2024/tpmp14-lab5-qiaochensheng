0a2a86dba2d3d048f2619fc5d53d9c2db3a28d03
qiaochensheng <159767314+qiaochensheng@users.noreply.github.com>
2024-04-15 10:25:35 +0300

    Update student.h

--- scr/student.h
+++ scr/student.h
@@ -1,1 +1,9 @@
-
+/* students.h */
+/* Author: {вместо скобок указать свои имя и фамилию} */
+/* lab4_zadacha2 */
+// Insert new record into an existing table
+void insertNewRecord(sqlite3 *db);
+// Delete records from an existing table
+void deleteRecord(sqlite3 *db);
+// Select records from an existing table
+void selectRecords(sqlite3 *db);
